56afff30e1cf114c4c57e36d2741f9d88a4308a5
midnight-rink <kkhh2323@gmail.com>
2024-07-23 01:13:10 +0900

    test commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = 'ko-kr' ##국가코드
 
 TIME_ZONE = 'UTC'
 
